07d7b92a0b9dd83bacd78cf5aac1f6f9aad9beea
Elena <marsik-123@mail.ru>
2022-11-21 19:30:46 +0300

    2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу,
+//  которая принимает на вход трёхзначное число 
+//  и на выходе показывает вторую цифру этого числа. 
+//  Выполнить с помощью числовых операций 
+//  (целочисленное деление, остаток от деления).
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = new Random().Next(455, 919);
+int firstDigit = number / 100; // 4
+int secondDigit = number % 100; //56
+int resaut = (secondDigit);
+int threeDigit = resaut /10; //5
+int fourDigit = resaut % 10; // 6
+Console.WriteLine($"{threeDigit}");
