7bdcf5278c7f00cacbf75179d97da513f52c303a
Elena <marsik-123@mail.ru>
2022-12-03 23:20:39 +0300

    16

---
+++ task15/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("0, 7, 8, -2, -2 ");
+Convert.ToInt32(Console.ReadLine());
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    } 
+     Console.WriteLine("]");
+}
+
+void SumIndexArray(int[] array)
+{
+    int i = new int ();
+    int sum = 0;
+    while (i >= 0);
+    {
+        sum = i++;
+        sum++;
+    }
+    return;
+}
+
+
+Console.WriteLine("0, 7, 8, -2, -2 ");
+return;
---
+++ task16/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n,
+//  заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+double[,] CreateMatrixRndDouble(double rows, double colums, double min, double max)
+
+{
+   double[,] matrix = new double[rows, columns]; //0, 1
+   int rows = Convert.ToInt32(Console.ReadLine());
+   Random rnd = new Random();
+   int columns = Convert.ToInt32(Console.ReadLine());
+   Random rnd = new Random();
+   double[,] matrix = new double[rows, columns]; //0, 1
+   for (double i = 0; i < matrix.GetLength(0); i++)
+   {
+    for (double j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rnd.Next(min, max + 1);
+    }
+   }
+   return matrix; 
+}
+void PrintMatrix(double[,] matrix)
+{
+    for (double i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (double j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix},");
+            else Console.Write($"{matrix}");
+        }
+        Console.Write("]");
+    }
+}
+
+double[,] array2D = CreateMatrixRndDouble(3, 4, -1, 9);
+PrintMatrix(array2D);
