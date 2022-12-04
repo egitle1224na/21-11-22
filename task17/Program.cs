5dece7ba7eb9a7e1c07fcf2c520070b2f0714267
Elena <marsik-123@mail.ru>
2022-12-04 20:12:40 +0300

    17

---
+++ task17/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write($"{matrix[i, j]}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+ int SumIndex (int[,]matrix)
+ {
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1);j++)
+        sum += (matrix[i, j] / 3);
+    }
+    return sum; 
+ }
+
+ int[,] arr=CreateMatrixRndInt (3, 4, 1, 9);
+ PrintMatrix(arr);
+ SumIndex(sum);
